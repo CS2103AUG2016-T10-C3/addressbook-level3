9235d784016a0367b1d36c0a78109b1617b6d6d1
khongyew <khongyew@gmail.com>
2016-09-26 23:21:29 +0800

    Add constructor to FindByTagCommand
    Add keywords attribute to FindByTagCommand

--- src/seedu/addressbook/commands/FindByTagCommand.java
+++ src/seedu/addressbook/commands/FindByTagCommand.java
@@ -1,5 +1,13 @@
 package seedu.addressbook.commands;
 
+import java.util.Set;
+
 public class FindByTagCommand {
     public static final String COMMAND_WORD = "findbytag";
+    
+    private final Set<String> keywords;
+    
+    public FindByTagCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
 }
