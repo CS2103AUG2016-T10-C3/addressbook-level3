9bd2f746433669a5c37c595745b0e4008b753697
zhiminglim <i-jovial@hotmail.com>
2016-09-27 20:57:23 +0800

    Edit DeleteCommand class

--- src/seedu/addressbook/commands/DeleteCommand.java
+++ src/seedu/addressbook/commands/DeleteCommand.java
@@ -16,27 +16,26 @@ public class DeleteCommand extends Command {
             + "Deletes the person identified by the index number used in the last person listing.\n\t"
             + "Parameters: INDEX\n\t"
             + "Example: " + COMMAND_WORD + " 1";
-
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
-
-
+    
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s"; 
+    public static final String MESSAGE_DELETE_PERSON_ABORT = "Person not deleted.";
+    
     public DeleteCommand(int targetVisibleIndex) {
         super(targetVisibleIndex);
     }
-
 
     @Override
     public CommandResult execute() {
         try {
             final ReadOnlyPerson target = getTargetPerson();
+            ConfirmCommand.showConfirm();
             addressBook.removePerson(target);
             return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, target));
-
         } catch (IndexOutOfBoundsException ie) {
             return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         } catch (PersonNotFoundException pnfe) {
             return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
         }
     }
-
+    
 }
